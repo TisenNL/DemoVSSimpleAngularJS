8f1f52a7990fc8df28d1d15e74a891b0fff996dc
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-10-30 08:46:47 -0300

    lesson 20

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,20 +1,9 @@
-﻿var app = angular
+﻿/// <reference path="angular.js" />
+
+var app = angular
         .module("myModule", [])
-        .controller("myController", function ($scope) {
+        .controller("myController", function ($scope, stringService) {
             $scope.transformString = function (input) {
-                if (!input)
-                    return input;
-
-                var output = "";
-
-                for (var i = 0; i < input.length; i++) {
-                    if (i > 0 && input[i] == input[i].toUpperCase()) {
-                        output = output + " ";
-                    }
-
-                    output = output + input[i];
-                }
-
-                $scope.output = output;
+                $scope.output = stringService.processString(input);
             };
         });
